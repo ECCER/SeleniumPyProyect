7a6106371c35f29c9645b49f887f41925f9b4499
Eduardo <educalli65@gmail.com>
2024-06-25 21:51:49 -0500

    Avance

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 service = Service('D:\WebDrivers\chromedriver-win64\chromedriver.exe')
 
 
 driver = webdriver.Chrome(service=service)
 
-driver.get("http://selenium.dev")
+driver.get("http://demoqa.com/text-box")
+driver.maximize_window()
+time.sleep(2)
 
-print(driver.title)
+nom = driver.find_element(By.XPATH, "//input[contains(@id, 'userName')]")
+nom.send_keys("Eccer")
+time.sleep(2)
 
 driver.quit()
